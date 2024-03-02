a18e31122305ef2b2602bdbcdc19dfdc40501e58
Aman Kumar <imtheaman@proton.me>
2024-03-03 00:39:19 +0530

    init: new Next.js upgrade

--- src/state/cell.type.ts
+++
@@ -1,8 +0,0 @@
-export type Id = string;
-export type Direction = "up" | "down";
-export type CellTypes = "code" | "text";
-export interface Cell {
-  id: Id;
-  type: CellTypes;
-  content: string;
-}
